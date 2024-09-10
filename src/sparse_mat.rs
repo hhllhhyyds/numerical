08235ecf95987d0aaae89d34a49f0a5169a5a84a
hhllhhyyds <hhllhhyyds@sina.com>
2024-09-10 09:33:50 +0800

    Add test examples to compare performance of Jacobi, Guass-Seidel and SOR Guass-Seidel methods

--- src/sparse_mat.rs
+++ src/sparse_mat.rs
@@ -108,6 +108,33 @@ impl<T: FloatCore> SparseMat<T> {
             x_k[ix_now] = (b[ix_now] - sum) / *self.get((ix_now, ix_now)).unwrap_or(&T::zero());
         }
     }
+
+    /// Transpose is needed before run gauss seidel iteration,
+    /// because this method rely on the sorted BTreeMap
+    pub fn sor_gauss_seidel_iterate(&self, x_k: &mut [T], b: &[T], omega: T) {
+        assert!(self.is_square());
+        let n = self.nx();
+        assert!(x_k.len() == n);
+        assert!(b.len() == n);
+
+        let mut iter = self.data.iter();
+        let mut elem = iter.next();
+        for ix_now in 0..n {
+            let mut sum = T::zero();
+            while let Some((&(ix, iy), &val)) = elem {
+                if ix == ix_now {
+                    if ix != iy {
+                        sum = sum + val * x_k[iy];
+                    }
+                } else {
+                    break;
+                }
+                elem = iter.next();
+            }
+            x_k[ix_now] = (T::one() - omega) * x_k[ix_now]
+                + omega * (b[ix_now] - sum) / *self.get((ix_now, ix_now)).unwrap_or(&T::zero());
+        }
+    }
 }
 
 impl<T> MatShape for SparseMat<T> {
@@ -186,6 +213,120 @@ mod tests {
     }
 
     #[test]
+    fn test_jacobi_iterate_2() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, -1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, -1.0));
+
+        let elems = elems_0.chain(elems_1);
+        let mat = SparseMat::new(n, n, elems.clone());
+
+        let b = {
+            let mut b = vec![1.; n];
+            b[0] = 2.;
+            b[n - 1] = 2.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let diag = vec![3.0; n];
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .map(|val| (val - 1.).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-6
+        {
+            iter_count += 1;
+            SparseMat::jacobi_iterate(&mut x, &b, &diag, &mat);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} jacobi iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let full_mat = SparseMat::new(n, n, elems.chain((0..n).map(|i| (i, i, 3.0))));
+        let y = full_mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x.iter().all(|y| y.abs_diff_eq(&1.0, 1e-6)));
+    }
+
+    #[test]
+    fn test_jacobi_iterate_3() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, 1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, 1.0));
+
+        let elems = elems_0.chain(elems_1);
+        let mat = SparseMat::new(n, n, elems.clone());
+
+        let b = {
+            let mut b = vec![0.; n];
+            b[0] = 1.;
+            b[n - 1] = -1.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let diag = vec![2.0; n];
+
+        let sol = (0..n)
+            .map(|i| if i % 2 == 0 { 1. } else { -1. })
+            .collect::<Vec<f64>>();
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .zip(sol.iter())
+            .map(|(a, b)| (a - b).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-3
+        {
+            iter_count += 1;
+            SparseMat::jacobi_iterate(&mut x, &b, &diag, &mat);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} jacobi iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let full_mat = SparseMat::new(n, n, elems.chain((0..n).map(|i| (i, i, 2.0))));
+        let y = full_mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x
+            .iter()
+            .zip(sol.iter())
+            .all(|(a, b)| a.abs_diff_eq(b, 1e-3)));
+    }
+
+    #[test]
     fn test_guass_seidel_iterate_0() {
         let mat = SparseMat::full_from_vec(3, 3, vec![3., 2., -1., 1., 4., 2., -1., 1., 5.]);
         let mat = mat.transpose();
@@ -200,5 +341,225 @@ mod tests {
         });
 
         (0..x.len()).for_each(|i| assert!(x[i].abs_diff_eq(&[2., -1., 1.][i], 1e-7)))
+    }
+
+    #[test]
+    fn test_guass_seidel_iterate_1() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, -1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, -1.0));
+        let elems_2 = (0..n).map(|i| (i, i, 3.0));
+
+        let elems = elems_0.chain(elems_1).chain(elems_2);
+        let mat = SparseMat::new(n, n, elems);
+
+        let b = {
+            let mut b = vec![1.; n];
+            b[0] = 2.;
+            b[n - 1] = 2.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .map(|val| (val - 1.).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-6
+        {
+            iter_count += 1;
+            mat.gauss_seidel_iterate(&mut x, &b);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} gauss seidel iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let y = mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x.iter().all(|y| y.abs_diff_eq(&1.0, 1e-6)));
+    }
+
+    #[test]
+    fn test_guass_seidel_iterate_2() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, 1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, 1.0));
+        let elems_2 = (0..n).map(|i| (i, i, 2.0));
+
+        let elems = elems_0.chain(elems_1).chain(elems_2);
+        let mat = SparseMat::new(n, n, elems.clone());
+
+        let b = {
+            let mut b = vec![0.; n];
+            b[0] = 1.;
+            b[n - 1] = -1.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let sol = (0..n)
+            .map(|i| if i % 2 == 0 { 1. } else { -1. })
+            .collect::<Vec<f64>>();
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .zip(sol.iter())
+            .map(|(a, b)| (a - b).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-3
+        {
+            iter_count += 1;
+            mat.gauss_seidel_iterate(&mut x, &b);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} gauss seidel iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let y = mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x
+            .iter()
+            .zip(sol.iter())
+            .all(|(a, b)| a.abs_diff_eq(b, 1e-3)));
+    }
+
+    #[test]
+    fn test_sor_guass_seidel_iterate_0() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, -1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, -1.0));
+        let elems_2 = (0..n).map(|i| (i, i, 3.0));
+
+        let elems = elems_0.chain(elems_1).chain(elems_2);
+        let mat = SparseMat::new(n, n, elems);
+
+        let b = {
+            let mut b = vec![1.; n];
+            b[0] = 2.;
+            b[n - 1] = 2.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .map(|val| (val - 1.).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-6
+        {
+            iter_count += 1;
+            mat.sor_gauss_seidel_iterate(&mut x, &b, 1.2);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} sor gauss seidel iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let y = mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x.iter().all(|y| y.abs_diff_eq(&1.0, 1e-6)));
+    }
+
+    #[test]
+    fn test_sor_guass_seidel_iterate_1() {
+        let n = 100;
+        let elems_0 = (0..(n - 1)).map(|i| (i + 1, i, 1.0));
+        let elems_1 = (0..(n - 1)).map(|i| (i, i + 1, 1.0));
+        let elems_2 = (0..n).map(|i| (i, i, 2.0));
+
+        let elems = elems_0.chain(elems_1).chain(elems_2);
+        let mat = SparseMat::new(n, n, elems.clone());
+
+        let b = {
+            let mut b = vec![0.; n];
+            b[0] = 1.;
+            b[n - 1] = -1.;
+            b
+        };
+
+        let mut x = vec![0.0; n];
+
+        let sol = (0..n)
+            .map(|i| if i % 2 == 0 { 1. } else { -1. })
+            .collect::<Vec<f64>>();
+
+        let start = std::time::Instant::now();
+
+        let mut iter_count = 0;
+        while x
+            .iter()
+            .zip(sol.iter())
+            .map(|(a, b)| (a - b).abs())
+            .reduce(f64::max)
+            .unwrap()
+            >= 1e-3
+        {
+            iter_count += 1;
+            mat.sor_gauss_seidel_iterate(&mut x, &b, 1.5);
+        }
+
+        println!(
+            "n = {n}, time used in {iter_count} sor gauss seidel iteration = {} ms",
+            (std::time::Instant::now() - start).as_millis()
+        );
+
+        let y = mat.mul_vec(&x);
+        println!(
+            "backward error = {:e}",
+            y.iter()
+                .zip(b.iter())
+                .map(|(m, n)| (m - n).abs())
+                .reduce(f64::max)
+                .unwrap()
+        );
+
+        assert!(x
+            .iter()
+            .zip(sol.iter())
+            .all(|(a, b)| a.abs_diff_eq(b, 1e-3)));
     }
 }
